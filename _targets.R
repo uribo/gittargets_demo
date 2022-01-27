55dba36d1c8c1e357d1173aca0cb12109b9a92b9
Shinya Uryu <r.sh1nya.uryu@gmail.com>
2022-01-27 22:01:41 +0900

    add new targets

---
+++ _targets.R
@@ -0,0 +1,21 @@
+library(targets)
+
+list(
+  tar_target(
+    pokemon_csv,
+    "https://gist.githubusercontent.com/simsketch/1a029a8d7fca1e4c142cbfd043a68f19/raw/bd584ee6c307cc9fab5ba38916e98a85de9c2ba7/pokemon.csv",
+    format = "url"
+  ),
+  tar_target(
+    df_raw,
+    readr::read_csv(pokemon_csv,
+                    col_types = "ddccccccccddddddddddddddd",
+                    n_max = 300)
+  )
+)
+
+
+
+
+
+
